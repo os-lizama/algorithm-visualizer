50d034cd14fc2209d894732f80373789328dbad0
os-lizama <oslizama.dev@gmail.com>
2023-03-23 17:40:25 -0600

    Modify the 'InsertSortScreen' file

--- app/src/main/java/com/oscarlizama/algorithmvisualizer/presentation/ui/algorithms/insertionsort/InsertionSortScreen.kt
+++ app/src/main/java/com/oscarlizama/algorithmvisualizer/presentation/ui/algorithms/insertionsort/InsertionSortScreen.kt
@@ -1,16 +1,47 @@
 package com.oscarlizama.algorithmvisualizer.presentation.ui.algorithms.insertionsort
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.oscarlizama.algorithmvisualizer.presentation.ui.algorithms.insertionsort.InsertionSortScreenViewModel.UIEvent.OnAlgorithmEvent
+import com.oscarlizama.algorithmvisualizer.presentation.ui.algorithms.insertionsort.InsertionSortScreenViewModel.UIEvent.OnStart
+import com.oscarlizama.algorithmvisualizer.presentation.uielement.BottomBarControls
+import com.oscarlizama.algorithmvisualizer.presentation.util.AlgorithmEvents
+import com.oscarlizama.algorithmvisualizer.ui.util.Spacer16
+import com.oscarlizama.data.algorithms.insertionsort.InsertionSort
 
 @Composable
-fun InsertionSortScreen() {
+fun InsertionSortScreen(
+    viewModel: InsertionSortScreenViewModel = InsertionSortScreenViewModel(insertionSort = InsertionSort())
+) {
+    LaunchedEffect(key1 = true) {
+        viewModel.onUiEvent(OnStart)
+    }
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(text = "Insertion Sort Algorithm")
+        Column(
+            Modifier.padding(horizontal = 8.dp)
+        ) {
+            Spacer16()
+            Text(text = "Insertion Sort Algorithm")
+            Spacer16()
+        }
+        BottomBarControls(
+            isPlaying = viewModel.uiState.isPlaying,
+            onPlayPause = { viewModel.onUiEvent(OnAlgorithmEvent(AlgorithmEvents.PlayPause)) },
+            onSlowDown = { viewModel.onUiEvent(OnAlgorithmEvent(AlgorithmEvents.SlowDown)) },
+            onSpeedUp = { viewModel.onUiEvent(OnAlgorithmEvent(AlgorithmEvents.SpeedUp)) },
+            onNextStep = { viewModel.onUiEvent(OnAlgorithmEvent(AlgorithmEvents.Next)) },
+            onPreviousStep = { viewModel.onUiEvent(OnAlgorithmEvent(AlgorithmEvents.Previous)) }
+        )
     }
 }
