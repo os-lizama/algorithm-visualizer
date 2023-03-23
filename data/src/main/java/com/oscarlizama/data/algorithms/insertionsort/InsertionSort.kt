707f3695bd01b842f4818ac67cbc5c12e6fae99e
os-lizama <oslizama.dev@gmail.com>
2023-03-23 09:43:56 -0600

    Create a new class called 'InsertionSort'

---
+++ data/src/main/java/com/oscarlizama/data/algorithms/insertionsort/InsertionSort.kt
@@ -0,0 +1,22 @@
+package com.oscarlizama.data.algorithms.insertionsort
+
+class InsertionSort {
+
+    suspend fun sort(
+        arr: IntArray,
+        onSwap: (IntArray) -> Unit
+    ) {
+        for (i in 1 until arr.size) {
+            var j = i - 1
+            val key = arr[i]
+            while (j >= 0 && key < arr[i]) {
+                arr[j + 1] = arr[j]
+                onSwap(arr)
+                j--
+            }
+            arr[j + 1] = key
+            onSwap(arr)
+        }
+    }
+
+}
