09d066cc151a59d1b7fd26c1e125419c90394967
os-lizama <oslizama.dev@gmail.com>
2023-03-23 09:49:25 -0600

    Add new route to the Algorithms NavGraph file

--- app/src/main/java/com/oscarlizama/algorithmvisualizer/presentation/navigation/navgraph/AlgorithmsNavGraph.kt
+++ app/src/main/java/com/oscarlizama/algorithmvisualizer/presentation/navigation/navgraph/AlgorithmsNavGraph.kt
@@ -2,15 +2,21 @@ package com.oscarlizama.algorithmvisualizer.presentation.navigation.navgraph
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.oscarlizama.algorithmvisualizer.presentation.navigation.ALGORITHMS_ROUTE
 import com.oscarlizama.algorithmvisualizer.presentation.navigation.Screen
+import com.oscarlizama.algorithmvisualizer.presentation.ui.algorithms.insertionsort.InsertionSortScreen
 
 fun NavGraphBuilder.algorithmsNavGraph(navController: NavController) {
     navigation(
         startDestination = Screen.InsertionSortScreen.route,
         route = ALGORITHMS_ROUTE
     ) {
-
+        composable(
+            route = Screen.InsertionSortScreen.route
+        ) {
+            InsertionSortScreen()
+        }
     }
 }
