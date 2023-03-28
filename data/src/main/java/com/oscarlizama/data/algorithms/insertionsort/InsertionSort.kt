ebe7562561d9b6abca5990c7b18c9ad4ccc2f7dc
os-lizama <oslizama.dev@gmail.com>
2023-03-28 10:38:23 -0600

    Modify the 'InsertionSort' class

--- data/src/main/java/com/oscarlizama/data/algorithms/insertionsort/InsertionSort.kt
+++ data/src/main/java/com/oscarlizama/data/algorithms/insertionsort/InsertionSort.kt
@@ -9,7 +9,7 @@ class InsertionSort {
         for (i in 1 until arr.size) {
             var j = i - 1
             val key = arr[i]
-            while (j >= 0 && key < arr[i]) {
+            while (j >= 0 && key < arr[j]) {
                 arr[j + 1] = arr[j]
                 onSwap(arr)
                 j--
