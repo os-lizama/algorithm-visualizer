a4b5c1c2c2dbead8faebf6904155e624c090b447
os-lizama <oslizama.dev@gmail.com>
2023-04-11 16:24:03 -0600

    Remove unused code from 'AlgorithmVisualizer' composable function

--- app/src/main/java/com/oscarlizama/algorithmvisualizer/presentation/uielement/AlgorithmVisualizer.kt
+++ app/src/main/java/com/oscarlizama/algorithmvisualizer/presentation/uielement/AlgorithmVisualizer.kt
@@ -6,19 +6,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.oscarlizama.algorithmvisualizer.ui.util.Spacer4
 
 @Composable
 fun AlgorithmVisualizer(
@@ -41,18 +36,17 @@ fun AlgorithmVisualizer(
             verticalAlignment = Alignment.Bottom,
         ) {
             arr.forEach {
-                /*Box(
-                    modifier = Modifier
-                        .height(it.dp)
-                        .width(10.dp)
-                        .background(Color.Green)
-                )
-                Spacer4()*/
                 Box(
                     modifier = Modifier
                         .height(if (it.dp > maxHeight.dp) maxHeight.dp else it.dp)
                         .width(itemWidth.dp)
-                        .background(Color.Red)
+                        .background(
+                            Color.Red,
+                            shape = RoundedCornerShape(
+                                topStart = 5.dp,
+                                topEnd = 5.dp
+                            )
+                        )
                 )
             }
         }
